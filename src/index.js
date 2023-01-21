254e6f1c1ad98f1a10116f641bdbf16c7c927609
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-01-20 23:02:53 -0300

    finalização de redirecionamentos para novo link

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <HashRouter>
+    <HashRouter>
       <App />
-      </HashRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
